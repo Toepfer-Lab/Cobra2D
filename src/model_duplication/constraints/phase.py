973499707aa5bc0eb9331aa7b5d170f2fc150562
Jan-Niklas <Jan-Niklas.Weder@live.de>
2022-03-28 13:55:05 +0200

    Possibility to customize individual reactions per phase
    NEW: Parameters of individual reactions can now be set per phase. These are automatically applied to the model while all other changes are applied as well.
    
    UPDATED: The XSD file has been adapted to the new requirements.

--- src/model_duplication/constraints/phase.py
+++ src/model_duplication/constraints/phase.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 from inspect import isclass
-from typing import Literal, List, Union
+from typing import List, Literal, Union
 from xml.etree.ElementTree import Element
 
-from cobra import Reaction, Model, DictList
+from cobra import DictList, Model, Reaction
 from prettytable import PrettyTable
 
 from model_duplication.duplication.duplication import _main_placeholder
@@ -22,39 +22,65 @@ class Phase:
     reaction_settings: List[Reaction]
 
     def __init__(
-        self,
-        id: str,
-        light_dark: Literal["light", "dark"],
-        timeframe: int = 1,
-        volume: int = 1,
-        name: str = "",
+            self,
+            id: str,
+            light_dark: Literal["light", "dark"],
+            timeframe: int = 1,
+            volume: int = 1,
+            name: str = "",
     ):
         self.id = id
         self.volume = volume
         self.name = name
         self.light_dark = light_dark
         self.timeframe = timeframe
+        self.reaction_settings = []
 
     def to_xml(self):
         element = Element("phase")
 
         element.set("id", self.id)
         element.set("volume", str(self.volume))
-        element.set("name", (self.name))
+        element.set("name", self.name)
         element.set("light_dark", self.light_dark)
         element.set("timeframe", str(self.timeframe))
 
+        for reaction in self.reaction_settings:
+            child = Element("reaction")
+            child.set("id", reaction.id)
+            child.set("lower_bound", str(reaction.lower_bound))
+            child.set("upper_bound", str(reaction.upper_bound))
+
+            element.append(child)
+
         return element
+
+    def add_reaction(self, reaction: Reaction):
+        self.reaction_settings.append(reaction)
 
     @classmethod
     def from_dict(cls, data: dict) -> Phase:
-        return cls(
-            id=data["id"],
-            volume=int(data["volume"]),
-            name=data["name"],
-            light_dark=data["light_dark"],
-            timeframe=int(data["timeframe"]),
+        output = cls(
+                id=data["id"],
+                volume=int(data["volume"]),
+                name=data["name"],
+                light_dark=data["light_dark"],
+                timeframe=int(data["timeframe"]),
         )
+
+        if "reaction" not in data.keys():
+            return output
+
+        for reaction in data["reaction"]:
+            new_reaction = Reaction(
+                    id=reaction["id"],
+                    lower_bound=reaction["lower_bound"],
+                    upper_bound=reaction["upper_bound"]
+            )
+
+            output.add_reaction(new_reaction)
+
+        return output
 
 
 class Phases:
@@ -67,7 +93,7 @@ class Phases:
         output = PrettyTable(["Phase", "Name", "Volume", "Timeframe"])
         for phase in self.phases:
             output.add_row(
-                [phase.id, phase.name, phase.volume, phase.timeframe]
+                    [phase.id, phase.name, phase.volume, phase.timeframe]
             )
 
         return output.get_string()
@@ -95,10 +121,16 @@ class Phases:
         phase_names = [phase.id for phase in self.phases]
 
         new_model = _main_placeholder(
-            model=model, labels=phase_names, file=None, genes=link_genes
+                model=model, labels=phase_names, genes=link_genes
         )
 
-        # ToDo change specified Reactions
+        for phase in self.phases:
+            for reaction in phase.reaction_settings:
+                reaction2adjust: Reaction = new_model.reactions.get_by_id(
+                        f"{reaction.id}_{phase.id}"
+                )
+                reaction2adjust.lower_bound = reaction.lower_bound
+                reaction2adjust.upper_bound = reaction.upper_bound
 
         return new_model
 
