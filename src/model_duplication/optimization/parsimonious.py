3d8255a69c0feb6b2e3df226d08551895dac698d
Jan-Niklas Weder <Jan-Niklas.Weder@live.de>
2022-06-14 21:49:51 +0200

    Implementation of a customized parsimonious FBA
    NEW: two new functions implemented by a customized version of the pFBA. This one ignores linker identified by '_L_'. All other reactions are minimized and the previous objective is set as constraint.
    
    NEW: Tests for the two new functions.

---
+++ src/model_duplication/optimization/parsimonious.py
@@ -0,0 +1,87 @@
+import re
+from itertools import chain
+from pprint import pprint
+from typing import Optional, Union, Dict, List
+
+from cobra import Model, Reaction, Solution
+from cobra.core import get_solution
+from cobra.util import fix_objective_as_constraint
+from optlang import Objective, Variable
+from optlang.symbolics import Zero
+
+from model_duplication import Constraints
+
+
+def adjusted_pfba(
+        constraints: Constraints,
+        model:Model = None,
+        fraction_of_optimum: float = 1.0,
+        objective: Optional[Union[Dict, Objective]] = None,
+        reactions: Optional[List[Reaction]] = None,
+) -> Solution:
+
+    """
+
+    Args:
+        constraints:
+        model:
+        fraction_of_optimum:
+        objective:
+        reactions:
+
+    Returns:
+
+    """
+    reactions = model.reactions if reactions is None else model.reactions.get_by_any(reactions)
+
+    with model as copy:
+        add_adjusted_pfba_objective(model = copy,
+                                    constraints = constraints,
+                                    objective=objective,
+                                    fraction_of_optimum=fraction_of_optimum)
+
+        copy.slim_optimize(error_value=None)
+        solution = get_solution(copy, reactions=reactions)
+    return solution
+
+def add_adjusted_pfba_objective(
+        constraints: Constraints,
+        model: Model,
+        objective: Optional[Union[Dict, Objective]] = None,
+        fraction_of_optimum: float = 1.0,
+) -> None:
+
+    if objective is not None:
+        model.objective = objective
+
+    if model.solver.objective.name == "_pfba_objective":
+        raise ValueError("The model already has a pFBA objective.")
+
+    fix_objective_as_constraint(model, fraction=fraction_of_optimum)
+
+    reaction_variables = (
+        (rxn.forward_variable, rxn.reverse_variable) for rxn in model.reactions
+    )
+    variables = chain(*reaction_variables)
+    model.objective = model.problem.Objective(
+        Zero, direction="min", sloppy=True, name="_pfba_objective"
+    )
+    linear_coefficients:Dict[Variable, int] = {}
+
+    reaction: Reaction
+    for reaction in model.reactions:
+        try:
+            # ToDo clear identification for Linker (Linker and Amino acids )
+            if re.match(r".*[^_]_L_[^_].*", reaction.id):
+                continue
+        except IndexError:
+            pass
+        phase_id = reaction.id[reaction.id.rindex('_')+1:]
+        phase = constraints.phases.phases.get_by_id(phase_id)
+        coeff = phase.timeframe * phase.volume
+
+        linear_coefficients[reaction.forward_variable] = coeff
+        linear_coefficients[reaction.reverse_variable] = coeff
+
+    model.objective.set_linear_coefficients(linear_coefficients)
+
---
+++ tests/optimization/test_parsimonious.py
@@ -0,0 +1,72 @@
+from pprint import pprint
+from unittest import TestCase
+
+import cobra
+from cobra import Configuration
+from cobra.io import read_sbml_model
+from cobra.util import linear_reaction_coefficients
+from importlib_resources import files, as_file
+
+from model_duplication import Constraints
+from model_duplication.optimization.parsimonious import add_adjusted_pfba_objective
+
+
+class Test(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cobra_config = Configuration()
+        cobra_config.solver = "glpk"
+
+        textbook_raw = files(cobra.data).joinpath("textbook.xml.gz")
+        with as_file(textbook_raw) as textbookXML:
+            cls.textbook = read_sbml_model(str(textbookXML))
+
+        ecoli_raw = files(cobra.data).joinpath("iJO1366.xml.gz")
+        with as_file(ecoli_raw) as ecoliXML:
+            cls.ecoli = read_sbml_model(str(ecoliXML))
+
+    def test_add_adjusted_pfba_objective(self):
+        model = self.ecoli.copy()
+        con = Constraints()
+        con.add_time_slots(3, 4, "light")
+        model = con.apply_to_model(model=model)
+
+        add_adjusted_pfba_objective(constraints=con,
+                                    model=model)
+
+        expected = "4.0*12DGR120tipp_default-0 + 4.0*12DGR120tipp_default-0_reverse_18e2b + 4.0*12DGR120tipp_default-1 + 4.0*12DGR120tipp_default-1_reverse_88a03 + 4.0*12DGR120tipp_default-2 + 4.0*12DGR120tipp_default-2_reverse_42b88 + 4.0*12DGR140tipp_default-0 + 4.0*12DGR140tipp_default-0_reverse_3d8d8 + 4.0*12DGR140tipp_default-1 + 4.0*12DGR140tipp_default-1_reverse_b7da9 + 4.0*12DGR140tipp_default-2 + 4.0*12DGR140tipp_default-2_reverse_479fe + 4.0*12DGR141tipp_default-0 + 4.0*12DGR141tipp_default-0_reverse_a4a7e + 4.0*12DGR141tipp_default-1 + 4.0*12DGR141tipp_default-1_reverse_ae833 + 4.0*12DGR141tipp_default-2 + 4.0*12DGR141tipp_default-2_reverse_970f8 + 4.0*12DGR160tipp_default-0 + 4.0*12DGR160tipp_default-0_reverse_6f278 + 4.0*12DGR160tipp_default-1 + 4.0*12DGR160tipp_default-1_reverse_d5dc0 + 4.0*12DGR160tipp_default-2 + 4.0*12DGR160tipp_default-2_reverse_9fb9b + 4.0*12DGR161tipp_default-0 + 4.0*12DGR161tipp_default-0_reverse_81cff + 4.0*12DGR161tipp_default-1 + 4.0*12DGR161tipp_default-1_reverse_"
+        self.assertEqual(expected, str(model.objective.expression)[:1000])
+        # TestCase 2
+
+        model = self.textbook.copy()
+        con = Constraints()
+        con.add_time_slots(3, 1, "light")
+        con.add_time_slots(2, 4, "light")
+        con.add_sub_models(["root", "leaf"], [1,3])
+
+        model = con.apply_to_model(model)
+
+        add_adjusted_pfba_objective(constraints=con,
+                                    model=model)
+        expected = "3.0*ACALD_leaf-0 + 3.0*ACALD_leaf-0_reverse_72443 + 3.0*ACALD_leaf-1 + 3.0*ACALD_leaf-1_reverse_da86c + 3.0*ACALD_leaf-2 + 3.0*ACALD_leaf-2_reverse_7a9cb + 12.0*ACALD_leaf-3 + 12.0*ACALD_leaf-3_reverse_daddf + 12.0*ACALD_leaf-4 + 12.0*ACALD_leaf-4_reverse_07b5d + 1.0*ACALD_root-0 + 1.0*ACALD_root-0_reverse_3e555 + 1.0*ACALD_root-1 + 1.0*ACALD_root-1_reverse_8c444 + 1.0*ACALD_root-2 + 1.0*ACALD_root-2_reverse_30749 + 4.0*ACALD_root-3 + 4.0*ACALD_root-3_reverse_2c4cf + 4.0*ACALD_root-4 + 4.0*ACALD_root-4_reverse_b0811 + 3.0*ACALDt_leaf-0 + 3.0*ACALDt_leaf-0_reverse_0f32d + 3.0*ACALDt_leaf-1 + 3.0*ACALDt_leaf-1_reverse_8f12e + 3.0*ACALDt_leaf-2 + 3.0*ACALDt_leaf-2_reverse_72061 + 12.0*ACALDt_leaf-3 + 12.0*ACALDt_leaf-3_reverse_dc5ef + 12.0*ACALDt_leaf-4 + 12.0*ACALDt_leaf-4_reverse_f1129 + 1.0*ACALDt_root-0 + 1.0*ACALDt_root-0_reverse_d4056 + 1.0*ACALDt_root-1 + 1.0*ACALDt_root-1_reverse_fd003 + 1.0*ACALDt_root-2 + 1.0*ACALDt_root-2_reverse_50e52 + 4.0*ACALDt_root-3 + 4.0*ACALDt_root-3_re"
+        self.assertEqual(expected, str(model.objective.expression)[:1000])
+        # TestCase 3 Linker
+
+        model = self.ecoli.copy()
+        con = Constraints()
+        con.add_time_slots(2, 2, "light")
+        con.add_linker_series("12dgr140_c")
+        model = con.apply_to_model(model=model)
+
+        add_adjusted_pfba_objective(constraints=con,
+                                    model=model)
+
+        self.assertEqual(10316, len(model.objective.variables))
+
+
+    def test_adjusted_pfba(self):
+        model = self.ecoli.copy()
+        con = Constraints()
+        con.add_time_slots(3, 4, "light")
+        model = con.apply_to_model(model=model)
+
